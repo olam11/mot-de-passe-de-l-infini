5110205ef9a1ae255b82f22c4dc1e3639164b2c4
olam11 <malo.gryspeerdt@gmail.com>
2024-06-15 10:44:49 +0200

    test3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,10 @@
 import streamlit as st
 
-@st.cache_data
-def config():
-    st.set_page_config(page_title="mot de passe de l'infini",page_icon=":infinity:")
+st.set_page_config(page_title="mot de passe de l'infini",page_icon=":infinity:")
     
-    
-
-config()
 if 'key' not in st.session_state:
     st.session_state.running = 0
+    
 st.session_state.running = st.session_state.running+1
 st.write(st.session_state.running)
 st.button("rerun")
